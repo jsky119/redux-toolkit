4c7c297c4cfa0a540f22760287385c9454d11ed7
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-10-14 18:16:37 +0900

    Create store.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+ReactDOM.render(
+  <Provide store={store}>
+    <App />
+  </Provide>,
+  document.getElementById("root")
+);
