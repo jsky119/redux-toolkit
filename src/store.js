98ed77635f89e3b18f1a23e88db6ff523a3122f0
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-10-25 19:11:33 +0900

    Refactor: Add createSlice, configureStore

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
 import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import {
+  createAction,
+  createReducer,
+  configureStore,
+  createSlice,
+} from "@reduxjs/toolkit";
 
 const ADD = "ADD";
 const DELETE = "DELETE";
@@ -18,8 +23,8 @@ const DELETE = "DELETE";
 //   };
 // };
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
 
 // const reducer = (state = [], action) => {
 //   switch (action.type) {
@@ -32,24 +37,39 @@ const deleteToDo = createAction("DELETE");
 //   }
 // };
 
-const reducer = createReducer([], {
-  /*createReducer를 이용하여 state를 mutate한다.
-  아래의 addToDo는 state를 return하지 않고 mutate한다
-  */
-  [addToDo]: (state, action) => {
-    //reducer의 비어있는 array에 새로운 toDo를 push한다. push는 아무것도 return하지 않으며 state를 mutate한다.
-    state.push({ text: action.payload, id: Date.now() });
+// const reducer = createReducer([], {
+//   /*createReducer를 이용하여 state를 mutate한다.
+//   아래의 addToDo는 state를 return하지 않고 mutate한다
+//   */
+//   [addToDo]: (state, action) => {
+//     //reducer의 비어있는 array에 새로운 toDo를 push한다. push는 아무것도 return하지 않으며 state를 mutate한다.
+//     state.push({ text: action.payload, id: Date.now() });
+//   },
+//   [deleteToDo]: (state, action) =>
+//     //반면 filter는 새로운 array를 return한다.
+//     state.filter((toDo) => toDo.id !== action.payload),
+// });
+
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    //반면 filter는 새로운 array를 return한다.
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = createStore(reducer);
+// export const actionCreators = {
+//   addToDo,
+//   deleteToDo,
+// };
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+console.log(toDos.reducer);
 
-export default store;
+export const { add, remove } = toDos.actions;
+
+// export default store;
+export default configureStore({ reducer: toDos.reducer });
